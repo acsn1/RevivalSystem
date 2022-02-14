a54921915d6817ac8ef72377eb1eace4abfdc48f
PrivateCode564 <98239215+PrivateCode564@users.noreply.github.com>
2022-02-14 19:06:06 +0200

    Update TaskManager.java

--- src/main/java/me/acsn/revivalsystem/task/TaskManager.java
+++ src/main/java/me/acsn/revivalsystem/task/TaskManager.java
@@ -18,11 +18,30 @@ public class TaskManager {
         new BukkitRunnable(){
 
             public void run(){
-
+                
                 for(Map.Entry<UUID, Long> entry:core.dead.entrySet()){
                     if(entry.getValue() < System.currentTimeMillis()){
                         Bukkit.getPlayer(entry.getKey()).setHealth(0.0);
                         core.dead.remove(entry.getKey());
+
+                    /*
+                    #This is if you want to remove the invisible armor-stand if the player gets forcefully killed (passing the 1 minute)        
+                    
+                    
+                    Player player = Bukkit.getPlayer(entry.getKey());
+                    for (Entity entities : player.getNearbyEntities(player.getLocation().getX(),
+                            player.getLocation().getY(), player.getLocation().getZ())) {
+
+                        if (entities.getType().equals(EntityType.ARMOR_STAND)) {
+                            if (entities.getCustomName() != null && entities.getCustomName().equals(player.getUniqueId().toString())) {
+                                entities.remove();
+                            }
+
+                        }
+                    
+                    
+                }*/
+
                     }
                 }
             }
